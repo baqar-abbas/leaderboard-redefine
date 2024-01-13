e351507e209d18f4429bef945080fb8fcfd39585
baqar-abbas <baqar007@hotmail.com>
2024-01-13 07:03:05 +0500

    setup project environment for leaderboard project.
    installed linters configurations for stylehint, eslint and webhint. Setup webpack configurations to use webpack as a module bundler.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+/* eslint-disable-next-line */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|jfif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+};
